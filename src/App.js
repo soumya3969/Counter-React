47cb4c379abeb1454dc78d1813ecab84e84e38ca
soumya <117825720+soumya3969@users.noreply.github.com>
2023-10-20 13:49:58 +0000

    counter completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Decrement from "./components/Decrement";
+import Increment from "./components/Increment";
+import Reset from "./components/Reset";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [isIncreased, setIsIncreased] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-[#ecfdf1] h-[100vh] flex justify-center items-center">
+      <div className="flex flex-col space-y-10">
+        <p className="text-[40px] font-serif text-center lg:text-[40px]">
+          Counter App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+      <div className="flex flex-col justify-center items-center space-y-8">
+        <p
+          className="text-[#000] text-[50px] font-medium"
+          style={{
+            color: isIncreased
+              ? "green"
+              : isIncreased === false
+              ? "red"
+              : "black",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {value}
+        </p>
+        <div className="flex flex-row gap-3 py-10 lg:gap-10">
+          <Increment i={value} i1={setValue} increase={setIsIncreased} />
+          <Reset r1={setValue} increase={setIsIncreased} />
+          <Decrement i={value} i1={setValue} increase={setIsIncreased} />
+        </div>
+      </div>
     </div>
   );
 }
