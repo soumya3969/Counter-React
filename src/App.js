47808bc9eb46a93690c845e2f2ebe59f62525f34
soumya <117825720+soumya3969@users.noreply.github.com>
2023-10-20 14:28:18 +0000

    counter app completed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import { useState } from "react";
-import Decrement from "./components/Decrement";
-import Increment from "./components/Increment";
-import Reset from "./components/Reset";
+import "./index.css";
 
 function App() {
-  const [value, setValue] = useState(0);
-  const [isIncreased, setIsIncreased] = useState(false);
-
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
-    <div className="bg-[#ecfdf1] h-[100vh] flex justify-center items-center">
-      <div className="flex flex-col space-y-10">
-        <p className="text-[40px] font-serif text-center lg:text-[40px]">
-          Counter App
-        </p>
-      </div>
-      <div className="flex flex-col justify-center items-center space-y-8">
-        <p
-          className="text-[#000] text-[50px] font-medium"
-          style={{
-            color: isIncreased
-              ? "green"
-              : isIncreased === false
-              ? "red"
-              : "black",
-          }}
-        >
-          {value}
-        </p>
-        <div className="flex flex-row gap-3 py-10 lg:gap-10">
-          <Increment i={value} i1={setValue} increase={setIsIncreased} />
-          <Reset r1={setValue} increase={setIsIncreased} />
-          <Decrement i={value} i1={setValue} increase={setIsIncreased} />
-        </div>
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count:{count}</p>
+      <div className="btns">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
       </div>
     </div>
   );
 }
-
 export default App;
